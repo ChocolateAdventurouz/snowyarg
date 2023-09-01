8ed327134c5b18f21d8ad5351802325713b2d900
Karapatakis Aggelos <aggeloskarapatakis@outlook.com.gr>
2023-09-01 20:25:02 +0300

    added module information

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import *
+
+setup(
+
+    name="snowyarg",
+    version="1.0.0",
+    author="Karapatakis Aggelos (ChocolateAdventurouz)",
+    description="A simple and basic argument parser library in Python",
+    packages=['snowyarg']
+)
